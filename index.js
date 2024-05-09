11be2e532a0c6f5bbda3b55b30bfe6b2ef9081d8
thexandersavage <swankylex@gmail.com>
2024-05-09 15:13:59 +0100

    added delete functionality method-override package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Path = require("path");
 const express = require("express");
-const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -13,6 +13,16 @@ const userRouter = require("./router/user");
 const viewRouter = require("./router/view");
 
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded form data
+app.use(
+  methodOverride(function(req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // Look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 app.use(express.json());
 app.set("view engine", "pug"); //SPECIFY VIEW ENGINE
 app.set("views", Path.join(__dirname, "views")); //SET PATH FOR VIEWS
